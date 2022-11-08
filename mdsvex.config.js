efb1205a42dca2472e53228ff23aeaaa3550237c
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-11-08 13:28:54 -0500

    [deps] bump

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,8 +1,8 @@
-// https://mdsvex.com/docs#options
+//? https://mdsvex.com/docs#options
 
-import autolinkHeadings from 'rehype-autolink-headings' // https://github.com/rehypejs/rehype-autolink-headings
-import slug from 'rehype-slug' // https://github.com/rehypejs/rehype-slug
-import abbr from 'remark-abbr' // https://github.com/zestedesavoir/zmarkdown/tree/master/packages/remark-abbr
+import autolinkHeadings from 'rehype-autolink-headings' //? https://github.com/rehypejs/rehype-autolink-headings
+import slug from 'rehype-slug' //? https://github.com/rehypejs/rehype-slug
+import abbr from 'remark-abbr' //? https://github.com/zestedesavoir/zmarkdown/tree/master/packages/remark-abbr
 
 const mdsvexConfig = {
 	extensions: ['.md', '.svx'],
